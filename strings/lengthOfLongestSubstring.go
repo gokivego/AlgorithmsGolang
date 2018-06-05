4228ce24c619a1ffe573b1b83fe4c06c3101fd5a
Venkat Palampally <gokivego@gmail.com>
2018-06-05 11:02:23 -0700

    Minor modifications

--- strings/lengthOfLongestSubstring.go
+++ strings/lengthOfLongestSubstring.go
@@ -9,16 +9,19 @@ import ("fmt"
 
 func lengthOfLongestSubstring(s string) int {
     charHash := make(map[string]int)
-    start := -1
+    start := 0
     maxLen := 0
     for idx,rune := range s {
         if valIdx,ok := charHash[string(rune)] ; ok {
-            start = valIdx
+            for _,rune := range s[start:valIdx] {
+                delete(charHash,string(rune))
+            }
+            start = valIdx + 1
             charHash[string(rune)] = idx
         } else {
             charHash[string(rune)] = idx
-            if maxLen < (idx - start) {
-                maxLen = idx - start
+            if maxLen < (idx - start + 1) {
+                maxLen = idx - start + 1
             }
         } 
     }
@@ -28,6 +31,7 @@ func lengthOfLongestSubstring(s string) int {
 func main() {
     //s1 := "abcabcbb"
     s2 := "abcdecdfghi"
+    //s2 := "tmmzuxt"
     length := lengthOfLongestSubstring(s2)
     fmt.Println("Length of Longest Substring in the string ",s2,"is :",length)
 }
