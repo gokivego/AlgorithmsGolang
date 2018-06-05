13bb107fe81495214b580b5632becd3f8fa71c5e
Venkat Palampally <gokivego@gmail.com>
2018-06-04 17:40:42 -0700

    Pythagorean Triplets

---
+++ MathAndStats/PythagoreanTriplets.go
@@ -0,0 +1,19 @@
+/*
+Pythagorean Triplets
+Given an array find all the pythagorean triplets (a^2 + b^2 = c^2)
+*/
+
+type triplet struct {
+    a,b,c int
+}
+
+func PythagoreanTripletsBruteForce(array) []triplets {
+    tripletArray := []triplet
+    for _, val
+}
+
+func main() {
+    array := []int{4,16,1,2,3,5,6,8,25,10}
+    tripletArray := PythagoreanTripletsBruteForce(array)
+    
+}
